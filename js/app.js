135874fd790233998195a100f4dd687ada9a3c1b
regan hayes <hayesregan@yahoo.com>
2021-12-07 13:34:43 -0500

    updated toc and added reading 7 notes

--- js/app.js
+++ js/app.js
@@ -4,12 +4,7 @@
 
 let seattleList = document.getElementById('seattle');
 
-// console.log(ul);
-
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12am', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
-
-// console.log(hours);
-
 
 let seattle = {
   location: 'Seattle',
